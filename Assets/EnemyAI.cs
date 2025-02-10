2b386daf0aa7dc51b40696209d0f426f613e56e3
ECTheHunter <emirhancatalbas@windowslive.com>
2025-02-11 00:51:19 +0300

    .

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -3,36 +3,71 @@ using Pathfinding;
 
 public class EnemyAI : MonoBehaviour
 {
-    private AIPath path;
+    private Path path;
     [SerializeField] private float movespeed;
     [SerializeField] private Transform target;
-    [SerializeField] public float health;
-    [SerializeField] public float damage;
+    [SerializeField] private float health;
+    [SerializeField] private float damage;
+    [SerializeField] private float nextpointdistance;
     private int currentwaypoint = 0;
-    private bool reachedpath = false;
+    public bool reachedpath = false;
     private Seeker seeker;
     private Rigidbody2D rb2D;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        path = GetComponent<AIPath>();
         seeker = GetComponent<Seeker>();
         rb2D = GetComponent<Rigidbody2D>();
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+        Physics2D.IgnoreLayerCollision(0, 6);
+
+    }
+    private void UpdatePath()
+    {
+        if (seeker.IsDone()) { seeker.StartPath(rb2D.position, target.position, OnPathComplete); }
+    }
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentwaypoint = 0;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        path.maxSpeed = movespeed;
-        path.destination = target.position;
+        if (path == null)
+        {
+            return;
+        }
+        if (currentwaypoint >= path.vectorPath.Count)
+        {
+            reachedpath = true;
+            return;
+        }
+        else
+        {
+            reachedpath = false;
+        }
+        Vector2 direction = ((Vector2)path.vectorPath[currentwaypoint] - rb2D.position).normalized;
+        Vector2 force = direction * movespeed * Time.deltaTime;
+        rb2D.AddForce(force);
+        float distance = Vector2.Distance(rb2D.position, path.vectorPath[currentwaypoint]);
+        if (distance < nextpointdistance)
+        {
+            currentwaypoint++;
+        }
+
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("sds");
-        if(collision.gameObject.tag == "Player"){
-            
+        if (collision.gameObject.tag == "Player")
+        {
             collision.gameObject.GetComponent<Health>().DoDamage(damage);
+            Destroy(gameObject);
         }
     }
 }
