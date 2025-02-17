9a8ce29515d6d3f1de3e22def8ea84a5f1cc3296
ECTheHunter <emirhancatalbas@windowslive.com>
2025-02-18 01:34:53 +0300

    .

--- Assets/Bouncer.cs
+++ Assets/Bouncer.cs
@@ -13,20 +13,26 @@ public class Bouncer : MonoBehaviour
     private Vector2 direction;
     [SerializeField] private bool cansplit = false;
     [SerializeField] private GameObject bouncerPrefab;
+    [SerializeField] private Transform spawnpoint1;
+    [SerializeField] private Transform spawnpoint2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
-        Physics2D.IgnoreLayerCollision(0, 6);
+        Physics2D.IgnoreLayerCollision(7, 6);
+        Physics2D.IgnoreLayerCollision(7, 7);
         rb2D = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction);
-        if (hit.collider != null)
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 1f);
+        Debug.DrawRay(transform.position, direction);
+        if (hit.collider == null)
         {
-            print(hit.collider.gameObject.name);
+            cansplit = false;
         }
         if (hit.collider.tag == "Border")
         {
@@ -36,6 +42,7 @@ public class Bouncer : MonoBehaviour
         {
             cansplit = false;
         }
+
     }
     // Update is called once per frame
     void FixedUpdate()
@@ -63,7 +70,10 @@ public class Bouncer : MonoBehaviour
                 {
                     var firstcontact = collision.ClosestPoint(transform.position);
                     var collisionNormal = ((Vector2)transform.position - firstcontact).normalized;
-                    Vector2 newVelocity = (Vector2.Reflect(rb2D.linearVelocity.normalized, collisionNormal)).normalized;
+                    Vector2 newVelocity = Vector2.Reflect(rb2D.linearVelocity.normalized, collisionNormal).normalized;
+                    direction = newVelocity;
+                    float angle = Mathf.Atan2(rb2D.linearVelocityY, rb2D.linearVelocityX) * Mathf.Rad2Deg;
+                    transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
                     StartCoroutine(BounceOperation(newVelocity));
                 }
 
@@ -74,9 +84,13 @@ public class Bouncer : MonoBehaviour
     }
     IEnumerator BounceOperation(Vector2 newVelocity)
     {
-        Vector2 spawnOffset = new Vector2(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f));
-        GameObject newBouncer1 = Instantiate(bouncerPrefab, (Vector2)transform.position + spawnOffset, Quaternion.identity);
-        GameObject newBouncer2 = Instantiate(bouncerPrefab, (Vector2)transform.position - spawnOffset, Quaternion.identity);
+
+        while (cansplit)
+        {
+            yield return null;
+        }
+        GameObject newBouncer1 = Instantiate(bouncerPrefab, (Vector2)spawnpoint1.position, Quaternion.identity);
+        GameObject newBouncer2 = Instantiate(bouncerPrefab, (Vector2)spawnpoint2.position, Quaternion.identity);
 
         Bouncer bouncerScript1 = newBouncer1.GetComponent<Bouncer>();
         Bouncer bouncerScript2 = newBouncer2.GetComponent<Bouncer>();
