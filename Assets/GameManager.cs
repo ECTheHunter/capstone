66ccb5e4d73317a880f5db2de898c377a49f7a48
ECTheHunter <emirhancatalbas@windowslive.com>
2025-02-26 15:29:33 +0300

    .

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public enum WEAPONMODE{
+        Pistolmode,
+        Machinegunmode
+    }
     public static GameManager Instance { get; private set; }
     public float healthvalue;
     public GameObject player;
     public float damage;
+    public int weaponmode = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/InputHandler.cs
+++ Assets/InputHandler.cs
@@ -2,18 +2,37 @@ using System;
 using UnityEngine;
 public class InputHandler : MonoBehaviour
 {
-    public int count;
     void Update()
     {
 
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            GameManager.Instance.weaponmode = (int)GameManager.WEAPONMODE.Pistolmode;
+        }
+         if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            GameManager.Instance.weaponmode = (int)GameManager.WEAPONMODE.Machinegunmode;
+        }
+
+
         if (Input.GetMouseButtonDown(0))
         {
-            LayerMask enemyLayer = LayerMask.GetMask("Enemy");
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, enemyLayer);
-            print(hit.collider.gameObject.name);
-            if (hit.collider.gameObject.tag == "Enemy")
+            if (GameManager.Instance.weaponmode == (int)GameManager.WEAPONMODE.Pistolmode)
             {
-                hit.collider.gameObject.GetComponent<EnemyValues>().EatDamage(GameManager.Instance.damage);
+                LayerMask enemyLayer = LayerMask.GetMask("Enemy");
+                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, enemyLayer);
+                if (hit.collider.gameObject.tag == "Enemy")
+                {
+                    hit.collider.gameObject.GetComponent<EnemyValues>().EatDamage(GameManager.Instance.damage);
+                }
+            }
+
+        }
+        if (Input.GetMouseButton(0))
+        {
+            if (GameManager.Instance.weaponmode == (int)GameManager.WEAPONMODE.Machinegunmode)
+            {
+                print("hold");
             }
         }
     }
