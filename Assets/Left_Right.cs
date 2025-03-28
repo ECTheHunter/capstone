7ac4c6c453c048eb2a40c80fa72982c25c1d9331
ECTheHunter <emirhancatalbas@windowslive.com>
2025-03-28 16:23:13 +0300

    Update Left_Right.cs

--- Assets/Left_Right.cs
+++ Assets/Left_Right.cs
@@ -2,20 +2,36 @@ using UnityEngine;
 
 public class Left_Right : MonoBehaviour
 {
-    public bool direction;
-    public float speed;
-    public float shootrate;
-    public GameObject projectile;
-    
+    [SerializeField] private bool directionleft;
+    [SerializeField] private float speed;
+    [SerializeField] private float shootrate;
+    [SerializeField] private GameObject projectile;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 dir = Vector3.zero;
+        if (directionleft)
+        {
+            dir = Vector2.left * speed * Time.deltaTime;
+        }
+        else
+        {
+            dir = Vector2.right * speed * Time.deltaTime;
+        }
+        transform.Translate(dir);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            directionleft = !directionleft;
+        }
     }
 }
