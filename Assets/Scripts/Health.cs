623730d0bf6169face9320f61112acfdf2abb34a
gatttaca01 <zkaya245@gmail.com>
2025-04-12 21:42:46 +0300

    health bar added

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,21 +1,55 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] public Image healthBarFill;
+    [SerializeField] public Gradient colorGradient;
+
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-   
+        Debug.Log($"Collided with: {collision.gameObject.name}, Tag: {collision.tag}");
+
+        // Eğer çarpışan nesne bir Enemy ise
+        if (collision.CompareTag("Projectile"))
+        {
+            EnemyValues enemyValues = collision.GetComponent<EnemyValues>();
+            if (enemyValues != null)
+            {
+                float damage = enemyValues.damage; // Enemy'nin damage değerini al
+                DoDamage(damage);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     public void DoDamage(float damage)
     {
         GameManager.Instance.healthvalue -= damage;
-        SoundManager.Instance.PlaySound(SoundManager.Instance.baseTakeDamageClip);
+        GameManager.Instance.healthvalue = Mathf.Clamp(GameManager.Instance.healthvalue, 0, 100);
+
+        if (GameManager.Instance.healthvalue == 0)
+        {
+            Die();
+        }
+
+        UpdateHealthBar();
+    }
+
+    public void UpdateHealthBar()
+    {
+        if (healthBarFill != null)
+        {
+            healthBarFill.fillAmount = GameManager.Instance.healthvalue / 100f;
+            healthBarFill.color = colorGradient.Evaluate(healthBarFill.fillAmount);
+        }
+        else
+        {
+            Debug.LogWarning("Health bar fill is not assigned in the Inspector!");
+        }
+    }
+
+    public void Die()
+    {
+        Debug.Log("Player has died.");
     }
 }
