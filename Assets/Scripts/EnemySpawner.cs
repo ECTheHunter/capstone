464694a32d409a8b5d3ea642b4baaaf1481d1e63
ECTheHunter <emirhancatalbas@windowslive.com>
2025-04-18 22:32:59 +0300

    Update EnemySpawner.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,11 +1,18 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private float regularenemycost;
+    [SerializeField] private float l_renemycost;
+    [SerializeField] private float bouncerenemycost;
+    [SerializeField] private float chomperenemycost;
     [SerializeField] private GameObject enemy;
     [SerializeField] private float spawnrate;
     [SerializeField] private float diffucultyrate;
+    [SerializeField] List<GameObject> spawnpoints = new List<GameObject>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
