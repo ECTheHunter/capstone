19615088fc56b17891ebc8709468fe6ff25f6638
gatttaca01 <zkaya245@gmail.com>
2025-04-26 16:46:00 +0300

    game over panel and high score panel added

---
+++ Assets/Scripts/GameOverPanel.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOverPanel : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+    public GameObject highScoresPanel;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI levelText;
+    public TMP_InputField nameInputField;
+
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameOverPanel.SetActive(false);
+        highScoresPanel.SetActive(false);
+
+        gameManager = GameManager.Instance;
+    }
+
+    private void Update()
+    {
+        if (gameManager != null && gameManager.healthvalue <= 0)
+        {
+            ShowGameOverPanel(gameManager.playerscore, gameManager.level);
+        }
+    }
+
+    public void ShowGameOverPanel(int score, int level)
+    {
+        Time.timeScale = 0f; 
+        gameOverPanel.SetActive(true);
+        scoreText.text = $"Score: {score}";
+        levelText.text = $"Level: {level}";
+    }
+
+    public void ShowHighScores()
+    {
+        gameOverPanel.SetActive(false);
+        highScoresPanel.SetActive(true);
+        // High Scores panel logic can be added here
+    }
+
+    public void BackToGameOver()
+    {
+        highScoresPanel.SetActive(false);
+        gameOverPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
